b4d8a47f0778f081a2a1e9dcb6a8f8cceca42c26
Kyle Long <kylel95@vt.edu>
2020-01-29 00:58:47 -0500

    First commit

---
+++ src/build/precache-manifest.9d01c5b1eed4231edb61fed460f50924.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ac2dbb0b50a2ffb8d941866198ada63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46dcd485e1f24b818eea",
+    "url": "/static/css/main.7b5f7dc8.chunk.css"
+  },
+  {
+    "revision": "437df5820b1c9d52af00",
+    "url": "/static/js/2.01af24d6.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.01af24d6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "46dcd485e1f24b818eea",
+    "url": "/static/js/main.3447716d.chunk.js"
+  },
+  {
+    "revision": "1a6842cff40cdbd7ee88",
+    "url": "/static/js/runtime-main.d7f357ff.js"
+  }
+]);
